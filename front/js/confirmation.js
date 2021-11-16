67f0524f41b5108c7a80ac62fcbbb7d86ce9f854
Larbi Chajai <larbi.ch@hotmail.fr>
2021-11-16 15:50:22 +0100

    fin

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,5 +2,9 @@ let command = localStorage.getItem('orderId');
 
 //affichage de l'id sur la page html
 let displayOrderId = document.getElementById('orderId');
-displayOrderId.innerHTML = `${command}`;
+if (command != null) {
+  displayOrderId.innerHTML = `${command}`;
+} else {
+  displayOrderId.innerHTML = "Il n'y aucun produit dans le panier";
+}
 localStorage.clear();
